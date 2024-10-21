ae3a2c82a28cc338ba2ac708dbc4f88d1bd95c33
Krishna Meda <62899740+EmperorNumerius@users.noreply.github.com>
2024-10-21 11:52:47 -0600

    finallly

---
+++ website/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['cloud-527jppsmt-hack-club-bot.vercel.app'],
+    },
+}
+
+module.exports = nextConfig
