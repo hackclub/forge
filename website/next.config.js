28b6b66d22542490a1ac49f567d728478dd21392
Krishna Meda <62899740+EmperorNumerius@users.noreply.github.com>
2025-01-24 10:33:58 -0700

    it should work i think

--- website/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['cloud-527jppsmt-hack-club-bot.vercel.app'],
-    },
-}
-
-module.exports = nextConfig
