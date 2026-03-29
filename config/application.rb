781ab82566b742c58cad8b3c40adcad5e2229675
TheUnknownHacker <128781393+The-UnknownHacker@users.noreply.github.com>
2026-03-29 15:02:58 +1100

    Exclude constraints from Zeitwerk autoload

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Forge
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[assets tasks constraints])
 
     # Enable Rack::Attack middleware
     config.middleware.use Rack::Attack
