de182eec116974765948bc65064a9f9cddb8e2f2
TheUnknownHacker <128781393+The-UnknownHacker@users.noreply.github.com>
2026-03-29 15:09:38 +1100

    Use Solid Cache instead of Redis for production cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" } }
+  config.cache_store = :solid_cache_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
