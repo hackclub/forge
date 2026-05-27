c6b2f8d1c7a80be5aa0827b8b3b570b4081b09b5
Aarav J <jha0003@kambryacollege.vic.edu.au>
2026-05-27 15:13:03 +1000

    Show current streak on leaderboard instead of longest (#254)

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -5,7 +5,8 @@ class LeaderboardController < ApplicationController
     referral_counts = Referral.group(:referrer_id).count
     hours_totals = compute_hours_totals
     streak_user_ids = User.kept.joins(:activity_days).distinct.pluck(:id)
-    streak_totals = streak_user_ids.to_h { |id| [ id, User.find(id).longest_streak ] }
+    streak_totals = streak_user_ids.to_h { |id| [ id, User.find(id).current_streak ] }
+    streak_totals.reject! { |_, v| v.zero? }
 
     referral_ids = referral_counts.sort_by { |_, v| -v }.first(LIMIT).map(&:first)
     hours_ids = hours_totals.select { |_, v| v > 0 }.sort_by { |_, v| -v }.first(LIMIT).map(&:first)
